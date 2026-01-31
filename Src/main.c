295bbcc2b6f82594003bae9ae53764ce2eb17901
Irfan <irfan-05@graduate.utm.my>
2026-01-31 15:16:54 +0800

    Initialize system and handle logic in main function

--- Src/main.c
+++ Src/main.c
@@ -1,1 +1,14 @@
+#include "stm32f4xx.h"
+#include "system_init.h"
+#include "interrupts.h"
+#include "logic.h"
 
+int main(void) {
+    System_Init();
+    Init_Interrupts();
+
+    while (1) {
+        Handle_Soil_Logic();
+        Handle_LDR_Logic();
+    }
+}
