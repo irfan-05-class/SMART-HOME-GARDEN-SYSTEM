d9c02957c567028d376354282d34042db00985c9
Irfan <irfan-05@graduate.utm.my>
2026-01-31 15:18:26 +0800

    Implement system initialization function
    Initialize system clock and enable GPIO and ADC peripherals.

--- Src/system_init.c
+++ Src/system_init.c
@@ -1,1 +1,11 @@
+#include "system_init.h"
+#include "stm32f4xx.h"
 
+void System_Init(void) {
+    // Clock enable
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN |
+                    RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIODEN;
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN | RCC_APB2ENR_SYSCFGEN;
+
+    // GPIO and ADC setup (same as your code)
+}
